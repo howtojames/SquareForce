65e263dd2b67ba03bb8f351436d16c2767d0569b
James Ruan <jrjamesruan@gmail.com>
2024-01-08 13:11:31 -0800

    picture test2

--- migrations/versions/1e1d10c524f1_.py
+++ migrations/versions/1e1d10c524f1_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: 1e1d10c524f1
-Revises: 
+Revises:
 Create Date: 2024-01-08 12:10:10.628079
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -27,6 +31,10 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    if environment == "production":
+       op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+
+
     op.create_table('products',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('title', sa.String(), nullable=False),
@@ -40,6 +48,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['sellerId'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+       op.execute(f"ALTER TABLE products SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
