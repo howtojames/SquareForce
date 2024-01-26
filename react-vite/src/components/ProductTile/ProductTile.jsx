aa4c956ec47e8f28ee597ade75384607cdb20292
James Ruan <jrjamesruan@gmail.com>
2024-01-26 07:30:21 -0800

    ad added

--- react-vite/src/components/ProductTile/ProductTile.jsx
+++ react-vite/src/components/ProductTile/ProductTile.jsx
@@ -3,21 +3,29 @@ import './ProductTile.css';
 
 function ProductTile({ product }){
 
-    //console.log('product', product)
-    //console.log('product.imageUrl', product.imageUrl)
+
     return (
         <div id='product-tile'>
-            <Link key={product.id} to={`/products/${product.id}`} >
-                <img src={product.image} id='product-image'/>
+
+            <Link id="link" key={product.id} to={`/products/${product.id}`} >
+                <div id="image-section">
+                    <img src={product.image} id='product-image'/>
+                </div>
             </Link>
-            <div className='tile-bottom'>
-                <div className='tile-bottom-left'>
+
+            <div id="description-section">
+                 <div className='tile-bottom-left'>
                     <Link key={product.id} to={`/products/${product.id}`} id='product-title'>
-                    <div id="product-tile-title">{product.title}</div>
+                        <div id="product-tile-title">{product.title}</div>
                     </Link>
                     <div id="product-tile-price">${product.price}</div>
                 </div>
             </div>
+
+
+
+
+
 
         </div>
     )
