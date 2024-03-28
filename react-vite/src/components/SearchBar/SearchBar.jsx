069a6fb7fb4a7c10889171d6f02bf96115cef63e
James Ruan <jrjamesruan@gmail.com>
2024-03-27 23:58:09 -0700

    search algorithm

--- react-vite/src/components/SearchBar/SearchBar.jsx
+++ react-vite/src/components/SearchBar/SearchBar.jsx
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react-redux";
 function SearchBar() {
     const [query, setQuery] = useState('');
 
+    //get a list of all products
+    //then make a sogirotihm that
+    //show s a list of any character strings that are matching
+
     return (
         <div id="search-bar-container">
             <div id="query-conainer">
@@ -14,7 +18,7 @@ function SearchBar() {
         </div>
     )
 
-};
+}
 
 
 export default SearchBar;
