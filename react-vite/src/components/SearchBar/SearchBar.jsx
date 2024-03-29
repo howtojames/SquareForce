488edbab47145cfe5c97874a42628d2f2d9de5da
James Ruan <jrjamesruan@gmail.com>
2024-03-28 17:56:18 -0700

    search bar algo

--- react-vite/src/components/SearchBar/SearchBar.jsx
+++ react-vite/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,24 @@
 import { useState, useEffect } from "react-redux";
-
+import { thunkGetAllProducts } from '../../redux/product';
+import { useDisaptch, useSelector } from 'react-redux';
 
 function SearchBar() {
+    const dispatch = useDisaptch();
+
     const [query, setQuery] = useState('');
 
     //get a list of all products
     //then make a sogirotihm that
     //show s a list of any character strings that are matching
+
+    useEffect( () => {
+        dispatch(thunkGetAllProducts());
+    }, [dispatch]);
+
+    const allProducts = useSelector(state => state.products);
+    console.log("allProducts in Searchbar", allProducts);
+
+
 
     return (
         <div id="search-bar-container">
