08182b7aa27629596b288c17828adf2a0f845c2b
James Ruan <jrjamesruan@gmail.com>
2024-04-02 23:23:51 -0700

    search container

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -56,8 +56,9 @@ function Navigation() {
             </NavLink>
           </div>
 
-
-          <SearchBar/>
+          <div id="nav-search-container">
+            <SearchBar/>
+          </div>
 
       </div>
 
