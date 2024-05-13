fa24ae2fc026581b88c5c5f25fd472d284838ae8
James Ruan <jrjamesruan@gmail.com>
2024-05-13 16:15:33 -0700

    checkout page added

---
+++ react-vite/src/components/Checkout/Checkout.jsx
@@ -0,0 +1,20 @@
+import { NavLink, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react-redux';
+import  '/Checkout.css';
+
+function Checkout() {
+
+
+    return (
+        <div id="checkout-container">
+
+            <div>Shipping Address</div>
+            {/* want to update address here */}
+            {/* display the user's address here */}
+        </div>
+    )
+
+}
+
+export default Checkout;
