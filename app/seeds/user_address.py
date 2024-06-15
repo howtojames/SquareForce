0dda1efacce4833b345f6db3e50401dfbdef85d2
James Ruan <jrjamesruan@gmail.com>
2024-06-15 09:52:18 -0700

    add see redo

--- app/seeds/user_address.py
+++ app/seeds/user_address.py
@@ -1,30 +1,11 @@
 from app.models import db, User, environment, SCHEMA
-
+from sqlalchemy.sql import text
 
 # Adds a demo user, you can add other users here if you want
 def seed_user_address():
-    print("INSIDE SEED USERS")
+    print("INSIDE SEED USER ADDRESS")
 
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
 
-    seller1 = User(
-        username='seller1', email='seller1@gmail.com', password='password')
-    james = User(
-        username='jruan', email='jruan@gmail.com', password='password')
-
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
-
-    db.session.add(seller1)
-    db.session.add(james)
-
-    db.session.commit()
 
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
