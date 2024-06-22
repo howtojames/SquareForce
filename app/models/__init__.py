90415c0b5de5e8664f8c35e546465687d540a712
James Ruan <jrjamesruan@gmail.com>
2024-06-21 23:18:00 -0700

    model init import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
-from .models import User
+#now it they can be imported from app.models
+from .models import User, User_Address, Product, CartProduct, Review
 from .db import environment, SCHEMA
