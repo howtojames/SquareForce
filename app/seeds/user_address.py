98ae67e8132c5c70bfe0d1b5d782dd1ff58c1eb7
James Ruan <jrjamesruan@gmail.com>
2024-06-24 23:47:20 -0700

    undo seed change

--- app/seeds/user_address.py
+++ app/seeds/user_address.py
@@ -49,8 +49,8 @@ def seed_user_address():
 # it will reset the primary keys for you as well.
 def undo_user_address():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.user_addresses RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM users"))
+        db.session.execute(text("DELETE FROM user_addresses"))
 
     db.session.commit()
